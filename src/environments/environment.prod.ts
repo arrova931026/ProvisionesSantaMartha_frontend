897f210e12f4f50e9accf8f54e5aef3b2ff29434
Armando Rojas Valdez <arrova931026@gmail.com>
2026-07-06 08:59:27 -0600

    Se desactiva OCR para llenar datos mediante INE en Prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   apiUrl: 'https://provisionessantamartha-services.onrender.com/api',
   googleClientId: '509130416997-v65lse6q7fcfoh8qhq8jbgi6anuu2k3p.apps.googleusercontent.com',
   /** Habilita/deshabilita el botón de Autocompletar con OCR en Mis Datos */
-  ocrAutocompletarEnabled: true
+  ocrAutocompletarEnabled: false
 };
